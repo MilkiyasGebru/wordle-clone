f597dcac1c0e17ea335017ef10d200eba89307c6
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-05-01 10:09:11 +0300

    Added a function to decorate guess and add it the guesses state

--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -1,6 +1,13 @@
 import {useEffect, useState} from "react";
 
+const deep_copy = (guesses)=>{
+    return guesses.map(guess => ({
+        ...guess,
+    }))
+}
+
 export const useWordle = () => {
+    const Solution = "aabbc"
     const [turn, setTurn] = useState(0);
     const [currentGuess, setCurrentGuess] = useState('');
     const [guesses, setGuesses] = useState([]);
@@ -8,6 +15,29 @@ export const useWordle = () => {
     const [isCorrect, setIsCorrect] = useState(false);
 
     const handleKeyUp = ({key}) => {
+        if (key === "Enter") {
+
+            if (currentGuess.length < 5){
+                console.log("Guess should be 5")
+                return
+            }
+
+            if (turn > 5){
+                console.log("Finished ")
+                return
+            }
+
+
+            if (history.includes(currentGuess)) {
+                console.log("Word has already been guessed")
+                return
+            }
+            const colored_guess = decorate_guess()
+            setTurn(x => x + 1)
+            setGuesses([...deep_copy(guesses), colored_guess])
+
+
+        }
 
         if (key === "Backspace") {
             setCurrentGuess(prev => prev.slice(0,-1));
@@ -21,6 +51,40 @@ export const useWordle = () => {
                 })
             }
         }
+    }
+
+    const decorate_guess = ()=>{
+
+        const freq_solution = new Map();
+        const colored_guess = []
+
+        for ( var i = 0; i<currentGuess.length; i++){
+            if (!freq_solution.get(Solution[i])){
+                freq_solution.set(Solution[i], 0)
+            }
+            freq_solution.set(Solution[i], freq_solution.get(Solution[i]) + 1);
+        }
+
+        for (var i=0; i<currentGuess.length; i++){
+            if (currentGuess[i] === Solution[i]){
+                freq_solution.set(Solution[i], freq_solution.get(Solution[i]) - 1);
+            }
+
+            colored_guess.push({
+                element: currentGuess[i],
+                color: currentGuess[i] === Solution[i]? "green": "grey"
+            })
+        }
+
+        for (var i=0; i<currentGuess.length; i++){
+            if (currentGuess[i] !== Solution[i] && freq_solution.get(currentGuess[i]) > 0){
+                colored_guess[i].color = "gold"
+                freq_solution.set(Solution[i], freq_solution.get(currentGuess[i]) - 1);
+            }
+        }
+
+        return colored_guess
+
     }
 
     useEffect(() => {
