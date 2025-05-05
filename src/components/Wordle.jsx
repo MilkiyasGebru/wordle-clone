453c17e193c5290954a489e1fc090dafcc34fca4
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-05-05 14:47:56 +0300

    when the game is won or lost, the modal is displayed

--- src/components/Wordle.jsx
+++ src/components/Wordle.jsx
@@ -1,25 +1,45 @@
 import useWordle from "../hooks/useWordle";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import Grid from "./Grid";
+import Keyboard from "./Keyboard";
+import Modal from "./Modal";
+import Navbar from "./Navbar";
 
 export default function Wordle(){
-    const {currentGuess, handleKeyUp, turn, isCorrect,guesses} = useWordle()
+    const {currentGuess, handleKeyUp, turn, isCorrect,guesses, greenCharacters, yellowCharacters} = useWordle()
+    const [showModal, setShowModal] = useState(false);
 
     useEffect(() => {
         window.addEventListener('keyup', handleKeyUp)
 
+        if (isCorrect){
+            setTimeout(()=>{
+                setShowModal(true);
+            },2000)
+            window.removeEventListener('keyup', handleKeyUp)
+        }
+
+        if (turn > 5) {
+            setTimeout(()=>{
+                setShowModal(true);
+            },2000)
+            window.removeEventListener('keyup', handleKeyUp)
+        }
 
         return ()=>{
             window.removeEventListener('keyup', handleKeyUp)
         }
 
-    }, [handleKeyUp]);
+    }, [handleKeyUp, turn, isCorrect]);
 
 
     return (
-        <div>
-            This is the current guess: {currentGuess}
+        <div className="flex flex-col gap-4 font-semibold">
+            <Navbar />
             <Grid currentGuess={currentGuess} guesses={guesses} turn={turn} />
+            <Keyboard yellowCharacters={yellowCharacters} greenCharacters={greenCharacters} />
+            {showModal && <Modal turn={turn} isCorrect={isCorrect}/>}
+
         </div>
     )
 }
