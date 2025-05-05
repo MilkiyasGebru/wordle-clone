0d9f4a609236cc27c66ac285689d02666f9f7591
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-05-05 14:47:05 +0300

    added the RowGrid in the Grid

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -3,9 +3,13 @@ import RowGrid from "./RowGrid";
 export default function Grid({currentGuess, guesses, turn}){
     return (
         <div className="flex justify-center flex-col items-center gap-2">
-            {guesses.map((guess, index) => (
-                <RowGrid key={index} />
-            ))}
+            {guesses.map((guess, index) => {
+                if (index === turn){
+
+                    return <RowGrid key={index} current_guess={currentGuess}/>
+                }
+                return <RowGrid key={index} guess={guess} />
+})}
         </div>
     )
 }
