a85e00b74c9ebe0bfa8ca3db15089876cd105936
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-05-07 19:24:36 +0300

    adjusted css styles

--- src/components/RowGrid.jsx
+++ src/components/RowGrid.jsx
@@ -1,9 +1,9 @@
 import {forwardRef} from "react";
 
 const styles = {
-    gray: {'--background-color': ' #afb19f'},
-    gold: {'--background-color': '#c9e70c'},
-    green: {'--background-color': '#33ff39'},
+    gray: {'--background-color': ' #787C7E'},
+    gold: {'--background-color': '#C9B458'},
+    green: {'--background-color': '#6AAA64'},
 }
 
 const RowGrid = forwardRef(({guess, current_guess},ref) => {
@@ -12,7 +12,7 @@ const RowGrid = forwardRef(({guess, current_guess},ref) => {
 
         <div className="flex gap-2" >
             {guess.map((element,index)=>(
-                <div key={index} style={{...styles[element.color],animationDelay:`${350*index}ms`}} className={`w-[60px] h-[60px]  rounded-md flex justify-center items-center uppercase animate-flip text-white`}>
+                <div key={index} style={{...styles[element.color],animationDelay:`${350*index}ms`}} className={`w-[60px] h-[60px] text-3xl  rounded-md flex justify-center items-center uppercase animate-flip text-white`}>
                     {element.element}
                 </div>
             ))}
@@ -26,13 +26,13 @@ const RowGrid = forwardRef(({guess, current_guess},ref) => {
             <div className="flex gap-2" ref={ref}>
                 {letters.map((letter, index)=>(
                     <div key={index}
-                         className={`w-[60px] h-[60px] border border-gray-600 rounded-md flex justify-center items-center uppercase animate-bounce-cell`}>
+                         className={`w-[60px] h-[60px] border-2 border-gray-700 rounded-md flex justify-center items-center uppercase animate-bounce-cell`}>
                         {letter}
                     </div>
                 ))}
                 {Array(5-letters.length).fill(null).map((_, index)=> (
                     <div key={index}
-                         className={`w-[60px] h-[60px] border rounded-md flex justify-center items-center uppercase border-gray-50 `}>
+                         className={`w-[60px] h-[60px] border rounded-md flex justify-center items-center uppercase border-gray-600 `}>
 
                     </div>
                 ))}
@@ -44,7 +44,7 @@ const RowGrid = forwardRef(({guess, current_guess},ref) => {
         <div className="flex  gap-2">
             {Array(5).fill(null).map((val, index) => {
                 return <div key={index}
-                            className="w-[60px] h-[60px] border border-gray-200 rounded-md flex justify-center items-center uppercase">
+                            className="w-[60px] h-[60px] border border-gray-400 rounded-md flex justify-center items-center uppercase">
 
                 </div>
             })}
