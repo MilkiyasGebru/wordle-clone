4c2a37128151f31f450600b062f077838a175f90
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-05-07 19:24:05 +0300

    Fixed the keyboard styles

--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -1,15 +1,15 @@
 import KeyboardRow from "./KeyboardRow";
 
-export default function Keyboard({yellowCharacters, greenCharacters}) {
+export default function Keyboard({yellowCharacters, greenCharacters, handleKeyUp}) {
     const rows = [
         ["Q","W","E","R","T","Y","U","I","O","P"],
         ["A","S","D","F","G","H","J","K","L"],
-        ["ENTER","Z","X","C","V","B","N","M","CLEAR"]
+        ["Enter","Z","X","C","V","B","N","M","Backspace"]
     ]
 
     return (
-        <div className="flex flex-col items-center gap-2 justify-center w-full">
-            {rows.map((row,index)=> <KeyboardRow row={row}  key={index} yellowCharacters={yellowCharacters}  greenCharacters={greenCharacters}/>)}
+        <div className="flex flex-col  gap-2 justify-center items-center w-fit mx-auto">
+            {rows.map((row,index)=> <KeyboardRow row={row}  key={index} yellowCharacters={yellowCharacters} handleKeyUp={handleKeyUp}  greenCharacters={greenCharacters}/>)}
         </div>
     )
 }
--- src/components/KeyboardRow.jsx
+++ src/components/KeyboardRow.jsx
@@ -1,25 +1,26 @@
 const bg_colors = {
-    "gray": "bg-gray-300",
-    "green":"bg-green-500",
-    "gold":"bg-amber-500",
+    "gray": "bg-gray-300 text-white",
+    "green":"bg-[#6AAA64] text-white",
+    "gold":"bg-[#C9B458] text-white",
+    "empty":"bg-[#D3D6DA] text-black",
 }
 
-export default function KeyboardRow({row, yellowCharacters, greenCharacters}) {
-    console.log(yellowCharacters, row, row[0],greenCharacters.has(row[0]))
+export default function KeyboardRow({row, yellowCharacters, greenCharacters, handleKeyUp}) {
     return (
         <div className="flex gap-2">
             {row.map((item,i)=>{
-                console.log(item, " this is row elements")
-                let x = ""
+                let x = "empty"
                 if (yellowCharacters.has(item)){
                     x = "gold"
-                    // alert(x)
                 }
                 if (greenCharacters.has(item)){
                     x = "green"
                 }
+                console.log("x",x,bg_colors[x])
                 return (
-                    <div className={`${bg_colors[x]} text-black text-2xl flex justify-center items-center h-[60px] border border-gray-200 px-3 py-2 rounded-md`} key={i}>
+                    <div style={{
+                        transitionDelay:"3000ms"
+                    }} className={`${bg_colors[x]} cursor-pointer uppercase text-black text-2xl flex justify-center items-center h-[60px] min-w-[50px] border border-gray-200 px-3 py-2 rounded-md `} key={i} onClick={(e)=>{handleKeyUp({key:item})}}>
                         {item}
                     </div>
                 )
