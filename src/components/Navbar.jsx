efe6baf250534666977f1e2bc829b261a2bbced6
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-05-15 10:10:36 +0300

    Fixed the font size of the Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 export default function Navbar() {
     return (
-        <nav className="h-[60px] w-full flex justify-center items-center  border-b-1 text-4xl">Wordle</nav>
+        <nav className="h-[60px] w-full flex justify-center items-center font-bold border-b-1 text-4xl">Wordle</nav>
     )
 }
