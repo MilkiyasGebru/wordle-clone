d5c954e7897b4144ffd48bc3b764b241e15bab3c
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-05-22 19:21:25 +0300

    made sure that the error messages are removed after 3 seconds

--- src/components/ErrorMessage.jsx
+++ src/components/ErrorMessage.jsx
@@ -1,7 +1,25 @@
-export default function ErrorMessage({error}) {
+import {useEffect} from "react";
+
+export default function ErrorMessage({errors, setErrors}) {
+
+    useEffect(() => {
+        const timer = setTimeout(()=>{
+            setErrors(errors.slice(1));
+        },5000)
+        return ()=>{
+            clearTimeout(timer)
+        }
+    }, [errors]);
+
+    console.log(errors)
+    const display_errors = [...errors].reverse();
     return (
-        <div className="absolute top-[5%] left-[50%] border p-4 border-gray-500 bg-black text-white -translate-x-1/2">
-            <p>{error}</p>
+        <div className="absolute top-[5%] left-[50%] flex flex-col gap-2 text-white -translate-x-1/2">
+            {display_errors.map((error,index) =>{
+                return (
+                    <div className="border p-4 border-gray-500 bg-black text-white  transition-opacity duration-500 opacity-100"    >{error}</div>
+                )
+            })}
         </div>
     )
 }
